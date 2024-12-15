307f9919e9f5a0fd285474a63199fe96159d2297
merrillmckee <merrillmckee@gmail.com>
2024-12-15 09:40:09 -0500

    updated gdown script

--- src/scripts/download_gdrive_data.py
+++ src/scripts/download_gdrive_data.py
@@ -6,21 +6,26 @@ def download_gdrive_data(url, output):
     gdown.download_folder(
         url=url,
         output=output,
-        quiet=True,
-        use_cookies=True,
+        quiet=False,
+        use_cookies=False,
         remaining_ok=True,
     )
 
 
 if __name__ == "__main__":
+    # Summary: only downloads 50 files per gdrive folder :(
+    #
     # Found gdown in a SO post on downloading gdrive files; I couldn't get other suggestions to work
     # Even this solution didn't seem to work on some of the initial attempts.
     #
-    # did help
+    # did not help
     # - in the venv/lib/.../gdown, modify "download_folder.py" MAX_NUMBER_FILES = 5000000  # from 50
+    # - update: on inspection, only 50 files per folder were downloaded after the change above
     #
     # may have helped?
     # - in the venv/lib/.../gdown, modify "download.py" by adding small sleep to get_url_from_gdrive_confirmation()
+    # - in ~/.cache/gdown delete a possibly malformed "cookies.txt" file
+    # - run with use_cookies=False
     #
 
     url_ = "https://drive.google.com/drive/folders/1SiafFkKtjbhMCwX7FPYA-FVKQ_gJ3NO-"
